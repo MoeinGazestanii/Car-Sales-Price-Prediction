9962ee702e1de91f99c2dac1749a6ac61b53ac7a
Moein Gazestani <you@example.commohammadmoein.hajihoseinigazestani@studenti.univr.it>
2024-01-06 10:09:00 +0330

    Modifying the file

--- Temporary.py
+++ Temporary.py
@@ -1,4 +1,15 @@
+#Import libraries
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
+import seaborn as sb
+
+#Running the data
+
+df = pd.read_csv('C:\\Users\\Lenovo\\Desktop\\Car_details.csv')
+df
+
+# Basic Information
+df.shape
+df.columns
+df.info()
