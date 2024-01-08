a5584ae06be4bcf3d71650eb24150559888da3fe
Moein Gazestani <you@example.commohammadmoein.hajihoseinigazestani@studenti.univr.it>
2024-01-08 11:35:19 +0330

    EDA modify

--- Temporary.py
+++ Temporary.py
@@ -10,6 +10,8 @@ import seaborn as sb
 
 df = pd.read_csv('C:\\Users\\Lenovo\\Desktop\\Car_details.csv')
 print(df)
+
+
 # Basic Information
 print(df.shape)     #The dataset contains 8128 samples and 13 columns.
 
@@ -18,6 +20,12 @@ print(df.info())
 print(df.head(10))
 # "Selling_Price" will be the dependent variable and the rest of the variables will be considered as independent variables.
 
+# Get the number of rows and columns
+num_rows, num_columns = df.shape
+
+# Print the number of rows and columns
+print(f"Number of rows: {num_rows}")
+print(f"Number of columns: {num_columns}")
 
 
 
@@ -62,7 +70,8 @@ df_copy= df.copy()
 df_copy['fuel'].replace({"CNG":"CNG/LPG", "LPG": "CNG/LPG"}, inplace= True)
 
 # Encoding Owner
-df_copy['owner'].replace({'Test Drive Car':0, 'First Owner':1, 'Second Owner':2, 'Third Owner':3, 'Fourth & Above Owner':3})
+df_copy['owner'].replace({'Test Drive Car':0, 'First Owner':1, 'Second Owner':2, 'Third Owner':3, 'Fourth & Above Owner':3}, inplace=True)
+df_copy['owner'] = pd.to_numeric(df_copy['owner'])
 
 df_copy1 = pd.get_dummies(df_copy[['fuel', 'transmission', 'seller_type']],drop_first=True)
 df_copy= pd.concat([df_copy, df_copy1], axis=1)
@@ -71,6 +80,103 @@ df_copy.drop(['fuel', 'transmission', 'seller_type'],axis=1, inplace=True)
 
 #drop some columns
 df_copy=df_copy.drop(columns=['torque','name'],axis=1)
+
+
+#outlier Detection
+def detect_outliers_iqr(data):
+    Q1 = np.percentile(data, 25)
+    Q3 = np.percentile(data, 75)
+    IQR = Q3 - Q1
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+
+    outliers_indices = []
+    for val in data:
+        if val < lower_bound or val > upper_bound:
+            outliers_indices.append(val)
+    return outliers_indices
+
+outliers_km_driven = detect_outliers_iqr(df['km_driven'])
+print("Outliers detected in 'km_driven' column using IQR method:", outliers_km_driven)
+
+outliers_mileage = detect_outliers_iqr(df['mileage'])
+print("Outliers detected in 'mileage' column using IQR method:", outliers_mileage)
+
+outliers_engine = detect_outliers_iqr(df['engine'])
+print("Outliers detected in 'engine' column using IQR method:", outliers_engine)
+
+outliers_max_power = detect_outliers_iqr(df['max_power'])
+print("Outliers detected in 'max_power' column using IQR method:", outliers_max_power)
+
+#correlation
+correlation = df_copy.corr()
+print(correlation)
+
+#Basic Visualization of int and float columns
+
+numeric_columns = []
+for col in df.columns:
+    if df[col].dtype in ['float64', 'int64']:
+        numeric_columns.append(col)
+
+
+for column in numeric_columns:
+    plt.figure(figsize=(14, 6))  # Initialize the figure
+
+    # Plot histogram in the first subplot
+    plt.subplot(1, 2, 1)
+    plt.hist(df[column], rwidth=0.5, bins=20, color='blue', alpha=0.7)
+    plt.title(f"Histogram of {column}")
+    plt.xlabel(column)
+    plt.ylabel("Frequency")
+
+    # Plot horizontal box plot in the second subplot
+    plt.subplot(1, 2, 2)
+    plt.boxplot(df[column], vert=False)
+    plt.title(f"Horizontal Boxplot of {column}")
+    plt.xlabel(column)
+    plt.ylabel("Values")
+    plt.show()
+
+
+    # Categorical values visualization
+    plt.figure(figsize=(8, 6))
+    plt.subplot(2,2,1)
+    value_counts = df['transmission'].value_counts()
+    plt.bar(value_counts.index, value_counts.values, color='skyblue')
+    plt.title('Frequency of Transmission Types')
+    plt.xlabel('Transmission Type')
+    plt.ylabel('Frequency')
+    plt.subplot(2,2,2)
+    value_counts2 = df['seller_type'].value_counts()
+    plt.bar(value_counts2.index, value_counts2.values, color='skyblue')
+    plt.title('Frequency of Seller Types')
+    plt.xlabel('Seller Type')
+    plt.ylabel('Frequency')
+    plt.subplot(2,2,3)
+    value_counts3=df['fuel'].value_counts()
+    plt.bar(value_counts3.index,value_counts3.values,color='skyblue')
+    plt.title('Frequency of Fuel Types')
+    plt.xlabel('Fuel Type')
+    plt.ylabel('Frequency')
+    plt.subplot(2,2,4)
+    value_counts4 = df['owner'].value_counts()
+    plt.bar(value_counts4.index, value_counts4.values, color='skyblue')
+    plt.title('Frequency of Owners')
+    plt.xlabel('Owner')
+    plt.ylabel('Frequency')
+    plt.show()
+
+
+
+
+
+
+
+
+
+
+
 
 
 
